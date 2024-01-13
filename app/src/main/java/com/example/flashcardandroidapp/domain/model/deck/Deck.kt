8c4e6300b80b1f1c7788b94821883782b3a75933
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-01-13 09:07:24 -0300

    set type string to LocalDateTime

--- app/src/main/java/com/example/flashcardandroidapp/domain/model/deck/Deck.kt
+++ app/src/main/java/com/example/flashcardandroidapp/domain/model/deck/Deck.kt
@@ -3,14 +3,15 @@ package com.example.flashcardandroidapp.domain.model.deck
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDateTime
 
 @Entity
-data class Deck(
+data class Deck (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     @ColumnInfo(name = "title")
     val title: String,
     @ColumnInfo(name = "create_data")
-    val createData: String
+    val createData: LocalDateTime
     //val UserID: Int
 )
