dc576718837dc2289ab6ac7b51e3dde1e36de51a
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-01-13 09:09:25 -0300

    added ViewModel

---
+++ app/src/main/java/com/example/flashcardandroidapp/viewModel/CardViewModel.kt
@@ -0,0 +1,76 @@
+package com.example.flashcardandroidapp.viewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.flashcardandroidapp.data.repository.CardRepository
+import com.example.flashcardandroidapp.domain.model.card.Card
+import com.example.flashcardandroidapp.domain.model.card.Feedback
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CardViewModel @Inject constructor(
+    private val repository: CardRepository
+) : ViewModel() {
+
+//    var card by mutableStateOf(Card(0,"", "", LocalDateTime.now() , Feedback.NONE, LocalDateTime.now(), 0))
+//        private set
+
+    var card by mutableStateOf<Card?>(null)
+
+    var cards by mutableStateOf<List<Card>>(emptyList())
+        private set
+
+    private var deletedCard: Card? = null
+
+    fun getAllCardsById(deckId: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllCardsById(deckId).collect { card ->
+                cards = card
+            }
+        }
+    }
+
+    fun getCardById(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            card = repository.getCardById(id)
+        }
+    }
+
+    fun updateCard(card: Card) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateCard(card)
+        }
+    }
+
+    fun insertCard(card: Card) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertCard(card)
+        }
+    }
+
+    fun deleteCard(card: Card) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertCard(card)
+        }
+    }
+
+    fun undoDeletedDeck() {
+        deletedCard?.let { card ->
+            viewModelScope.launch(Dispatchers.IO) {
+                repository.insertCard(card)
+            }
+        }
+    }
+
+    // Update Feedback
+    fun updateFeedback(newFeedback: Feedback) {
+        card = card?.copy(feedback = newFeedback)
+    }
+}
---
+++ app/src/main/java/com/example/flashcardandroidapp/viewModel/DeckViewModel.kt
@@ -0,0 +1,65 @@
+package com.example.flashcardandroidapp.viewModel
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flashcardandroidapp.data.repository.DeckRepository
+import com.example.flashcardandroidapp.domain.model.deck.Deck
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+
+@RequiresApi(Build.VERSION_CODES.O)
+@HiltViewModel
+class DeckViewModel(
+    private val repository: DeckRepository
+): ViewModel() {
+    var deck by mutableStateOf(Deck(0, "", LocalDateTime.now()))
+        private set
+
+    val getAllDecks = repository.getAllDecks()
+
+    private var deletedDeck: Deck? = null
+
+    fun getDeckById(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            deck = repository.getDeckById(id = id)
+        }
+    }
+
+    fun insertDeck(deck: Deck) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertDeck(deck)
+        }
+    }
+
+    fun updateDeck(deck: Deck) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateDeck(deck)
+        }
+    }
+
+    fun deleteDeck(deck: Deck) {
+        viewModelScope.launch(Dispatchers.IO) {
+            deletedDeck = deck
+            repository.deleteDeck(deck)
+        }
+    }
+
+    fun undoDeletedDeck() {
+        deletedDeck?.let { deck ->
+            viewModelScope.launch(Dispatchers.IO) {
+                repository.insertDeck(deck)
+            }
+        }
+    }
+
+    fun updateTitle(newTitle: String) {
+        deck = deck.copy(title = newTitle)
+    }
+}
